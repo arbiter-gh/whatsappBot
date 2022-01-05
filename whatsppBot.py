49f5936b9ba9c429303a3956f615bb8b87788e4b
Rahul Mandal <rahulkumar397222@gmail.com>
2022-01-05 21:48:38 +0530

    updated

--- whatsppBot.py
+++ whatsppBot.py
@@ -4,14 +4,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def wBot(path = '//span[@title = "wBot"]'):
-    driver = webdriver.Firefox()
-    driver.get("https://web.whatsapp.com/")
-    try:
-        wbot = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH,path)))
-        wbot = driver.find_element_by_xpath(path)
-        wbot.click()
-        msg = driver.find_element_by_xpath('//div[@class = "_3FRCZ copyable-text selectable-text"][@data-tab = "1"]')
-    finally: 
-       pass
-wBot()
+driver = webdriver.Firefox()
+driver.get("https://web.whatsapp.com/")
+driver.implicitly_wait(20)
+def start_messaging(path = '//span[@title = "wBot"]'):
+    wbot = driver.find_element_by_xpath(path)
+    wbot.click()   
+    msg = driver.find_element_by_xpath('//div[@class = "_13NKt copyable-text selectable-text"][@data-tab = "9"]')
+    with open('wallE.txt','r') as f:
+        for word in f :
+            msg.send_keys(word)
+
+start_messaging()
